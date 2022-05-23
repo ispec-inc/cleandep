cac052fb6c9762ac2a3f95aec3a882eb24c0af22
Masaya Hayashi <mcdonalds.only@gmail.com>
2022-05-24 00:34:46 +0900

    refactor: optimize by deleting inspector dependency

--- cleandep.go
+++ cleandep.go
@@ -1,26 +1,21 @@
 package cleandep
 
 import (
-	"go/ast"
 	"os"
 	"strconv"
 
 	"github.com/goccy/go-yaml"
 	"github.com/mattn/go-zglob"
 	"golang.org/x/tools/go/analysis"
-	"golang.org/x/tools/go/analysis/passes/inspect"
-	"golang.org/x/tools/go/ast/inspector"
 )
 
 const doc = "cleandep detects illegal dependencies"
 
 var Analyzer = &analysis.Analyzer{
-	Name: "cleandep",
-	Doc:  doc,
-	Run:  run,
-	Requires: []*analysis.Analyzer{
-		inspect.Analyzer,
-	},
+	Name:     "cleandep",
+	Doc:      doc,
+	Run:      run,
+	Requires: []*analysis.Analyzer{},
 }
 
 func decodeConfig(filename string) (Config, error) {
@@ -50,22 +45,17 @@ func run(pass *analysis.Pass) (interface{}, error) {
 
 	src := pass.Pkg.Path()
 
-	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-	nodeFilter := []ast.Node{
-		(*ast.ImportSpec)(nil),
-	}
-	inspect.Preorder(nodeFilter, func(n ast.Node) {
-		switch n := n.(type) {
-		case *ast.ImportSpec:
-			dst, err := strconv.Unquote(n.Path.Value)
+	for _, file := range pass.Files {
+		for _, imp := range file.Imports {
+			dst, err := strconv.Unquote(imp.Path.Value)
 			if err != nil {
-				return
+				continue
 			}
 
 			for srcPattern, dstPatterns := range rules {
 				srcMatched, err := zglob.Match(srcPattern, src)
 				if err != nil {
-					return
+					continue
 				}
 				if !srcMatched {
 					continue
@@ -73,15 +63,15 @@ func run(pass *analysis.Pass) (interface{}, error) {
 				for _, dstPattern := range dstPatterns {
 					dstMatched, err := zglob.Match(dstPattern, dst)
 					if err != nil {
-						return
+						continue
 					}
 					if dstMatched {
-						pass.Reportf(n.Pos(), "package '%s' cannot depend on package '%s'", src, dst)
+						pass.Reportf(imp.Pos(), "package '%s' cannot depend on package '%s'", src, dst)
 					}
 				}
 			}
 		}
-	})
+	}
 
 	return nil, nil
 }
